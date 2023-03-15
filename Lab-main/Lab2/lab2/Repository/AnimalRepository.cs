feb78312951b5a7d55fa418b180b504a75326ad6
nlukatkin <klukatkin02@bk.ru>
2023-03-15 14:30:30 +0300

    Lab2

---
+++ Lab-main/Lab2/lab2/Repository/AnimalRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AnimalRepository : RepositoryBase<Animal>, IAnimalRepository
+    {
+        public AnimalRepository(RepositoryContext repositoryContext): base(repositoryContext)
+        {
+        }
+    }
+}
