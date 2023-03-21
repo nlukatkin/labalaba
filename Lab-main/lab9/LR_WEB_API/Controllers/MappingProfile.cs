6308b57ca6aae08581688ed1e8097c304ec67ad9
nlukatkin <klukatkin02@bk.ru>
2023-03-21 21:33:55 +0300

    lab9

---
+++ Lab-main/lab9/LR_WEB_API/Controllers/MappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Company, CompanyDto>() .ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        CreateMap<Employee, EmployeeDto>();
+        CreateMap<Order, OrderDto>();
+        CreateMap<Warehouse, WarehouseDto>();
+
+        CreateMap<CompanyForCreationDto, Company>();
+
+        CreateMap<EmployeeForCreationDto, Employee>();
+
+        CreateMap<WarehouseForCreationDto, Warehouse>();
+
+        CreateMap<OrderForCreationDto, Order>();
+
+        CreateMap<EmployeeForUpdateDto, Employee>();
+
+        CreateMap<CompanyForUpdateDto, Company>();
+
+        CreateMap<EmployeeForUpdateDto, Employee>().ReverseMap();
+
+        CreateMap<OrderForUpdateDto, Order>();
+
+        CreateMap<WarehouseForUpdateDto, Warehouse>();
+
+        CreateMap<OrderForUpdateDto, Order>().ReverseMap();
+
+    }
+
+
+}
